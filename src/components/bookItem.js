74ee959a934cc76f0a97543fbb5d5eaa1734702b
Abdoul Diallo <committed1@gmail.com>
2019-03-05 15:36:00 -0500

    Update BookItem card

--- src/components/bookItem.js
+++ src/components/bookItem.js
@@ -4,42 +4,28 @@ import { Card, Button, Header, Icon, Image } from 'semantic-ui-react'
 class BookItem extends React.Component {
         
     render(){
-        const { eng_title, arabic_title, about, language, translator, num_pages, file, image } = this.props.book
+        const { eng_title, arabic_title, about, language, translator, num_pages, file, image, author } = this.props.book
         const englishTitle = eng_title.toLowerCase()
                             .split(' ').map((s) => s.charAt(0)
                             .toUpperCase() + s.substring(1))
                             .join(' ');
         return(
-        <div className="ui card link">
-                <Card>
-                    <Card.Content extra>
-                        <div>
-                            <Header style={{ color: "black" }}><strong className="title">{englishTitle}</strong></Header>
-                        </div>
-                    </Card.Content>
-                    <Card.Content>
-                        <Image floated="left" className="ui small rounded image" src={image} />
-                        <div className="book-info" floated="right">
-                            
-                            <Card.Meta><strong className="arabic" style={{color: "black"}}>{arabic_title}</strong></Card.Meta>
-                            {/* <br/> */}
-                            <Card.Meta><p className="metaText">Language: {language}</p></Card.Meta>
-                            {/* <br/> */}
-                            <Card.Meta><p className="metaText">Pages: {num_pages}</p></Card.Meta>
-                        </div>
-                    </Card.Content>
-                    <Card.Content extra>
-                        <div>
-                        <a href={file} target="_blank">
-                                <Button compact size='mini' positive color='green'>
-                                <Icon name="download"/>Download
+            <div className="ui link row">
+                <div className="column-left"><img align='left' className="ui small rounded image" src={image} /></div>
+                <div className="column">
+                    <p className="title">{englishTitle}</p>
+                    <p className="title">{arabic_title}</p>
+                    {/* <span><p className="meta">Author:</p> <p>{author.name}</p></span> */}
+                    <span><p className="meta">Language:</p> <p>{language}</p></span><br/>
+                    <span><p className="meta">Pages:</p> <p>{num_pages}</p></span>
+                    <a href={file} target="_blank">
+                        <Button compact size='mini' basic positive>
+                            <Icon name="download" />Download
                             </Button>
-                        </a>
-                        </div>
-                    </Card.Content>
-                </Card>
-                <br/>
-        </div>
+                    </a>
+                </div>
+                <br />
+            </div>
         )
     }
 }
